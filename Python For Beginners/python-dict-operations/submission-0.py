df17962e8ea27a2669ad94f70b2c6a699c39b101
alexjamka <alexjamka3@gmail.com>
2026-06-13 10:26:42 -0600

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,15 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+print(your_dict)
+# a is the key
+print(your_dict["a"])
+
+print("d" in your_dict) # prints true 
+
+your_dict["a"] = 4
+print(your_dict)
+
